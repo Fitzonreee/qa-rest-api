105fa8f4393b0d66b6d5e2de200b6bd909ec980e
Fitzonreee <kevin.fitzhenry@createthenext.com>
2016-07-02 19:48:09 -0700

    Added error handling for 404 and 500

--- app.js
+++ app.js
@@ -16,15 +16,23 @@ app.use(jsonParser());
 // sends this url to routes file
 app.use("/questions", routes);
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
 
+// Error handler
 
-
-
-
-
-
-
-
+app.use(function(err, req, res, next){
+  res.status(err.status || 500);
+  res.json({
+    error: {
+      message: err.message
+    }
+  });
+});
 
 // ************* server port ***************//
 var port = process.env.PORT || 3000;
