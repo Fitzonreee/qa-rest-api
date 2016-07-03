fa0de0a64dc111c17ce9f05cc3e7ce10b8f1393b
Fitzonreee <kevin.fitzhenry@createthenext.com>
2016-07-02 19:28:58 -0700

    Added routes.js file and configured all routes for CRUD operations for both questions and answers

--- app.js
+++ app.js
@@ -2,7 +2,31 @@
 
 var express = require("express");
 var app = express();
+var routes = require("./routes");
+var jsonParser = require("body-parser").json;
+var logger = require("morgan");
 
+app.use(logger("dev"));
+// gives colorful status codes about API responses
+
+app.use(jsonParser());
+// needs to come before anything with body-parser
+
+
+// sends this url to routes file
+app.use("/questions", routes);
+
+
+
+
+
+
+
+
+
+
+
+// ************* server port ***************//
 var port = process.env.PORT || 3000;
 
 app.listen(port, function() {
---
+++ routes.js
@@ -0,0 +1,72 @@
+'use strict';
+
+var express = require("express");
+var router = express.Router();
+
+// ***** Questions Routes ***** //
+// -- strips away what was already matched in app.js --
+
+// GET /questions -- get all questions
+router.get("/", function(req, res) {
+  res.json({response: "You sent me a GET request"});
+});
+
+// POST /questions -- create question
+router.post("/", function(req, res) {
+  res.json({
+    response: "You sent me a POST request",
+    body: req.body
+  });
+});
+
+// GET /questions/:qID -- specific question
+router.get("/:qID", function(req, res) {
+  res.json({response: "You sent me a GET requestf or ID:" + req.params.qID});
+});
+
+// ***** Answer Routes ***** //
+
+// POST /questions/:qID/answers -- creating answer
+router.post("/:qID/answers", function(req, res) {
+  res.json({
+    response: "You sent me a POST request to /answers",
+    questionId: req.body.qID,
+    body: req.body
+  });
+});
+
+// PUT /questions/:qID/answers/:aID
+// edit a specific answer
+router.put("/:qID/answers/:aID", function(req, res) {
+  res.json({
+    response: "You sent me a PUT request to /answers",
+    questionId: req.params.qID,
+    answerID: req.params.aID,
+    body: req.body
+  });
+});
+
+// DELETE /questions/:qID/answers/:aID
+// delete a specific answer
+router.delete("/:qID/answers/:aID", function(req, res) {
+  res.json({
+    response: "You sent me a DELETE request to /answers",
+    questionId: req.params.qID,
+    answerID: req.params.aID
+  });
+});
+
+// POST /questions/:qID/answers/:aID/vote-up
+// POST /questions/:qID/answers/:aID/vote-down
+// vote a specific answer
+router.post("/:qID/answers/:aID/vote-:dir", function(req, res) {
+  res.json({
+    response: "You sent me a POST request to /vote-" + req.params.dir,
+    questionId: req.params.qID,
+    answerID: req.params.aID,
+    vote: req.params.dir
+  });
+});
+
+
+module.exports = router;
