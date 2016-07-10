e3cbdc536df5a8be855a50d8877c36d6c4f847d6
Fitzonreee <kevin.fitzhenry@createthenext.com>
2016-07-10 15:19:43 -0700

    Added pre save callback to QuestionSchema and added a sorting function to sort answers by upvotes

--- models.js
+++ models.js
@@ -4,6 +4,13 @@ var mongoose = require("mongoose");
 
 var Schema = mongoose.Schema;
 
+var sortAnswers = function(a, b){
+  if (a.votes === b.votes) {
+    return b.updatedAt - a.updatedAt;
+  }
+  return b.votes - a.votes;
+};
+
 var AnswerSchema = new Schema({
   text: String,
   createdAt: {type: Date, default: Date.now},
@@ -15,6 +22,12 @@ var QuestionSchema = new Schema({
   text: String,
   createdAt: {type: Date, default: Date.now},
   answers: [AnswerSchema]
+});
+
+// Pre-save callback
+QuestionSchema.pre("save", function(next){
+  this.answers.sort(sortAnswers);
+  next();
 });
 
 var Question = mongoose.model("Question", QuestionSchema);
